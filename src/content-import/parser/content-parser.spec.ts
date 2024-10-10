7333e4f7f728116183a821beb0a45c18cbc1032e
BearMett <agomdole@gmail.com>
2024-10-10 10:39:50 +0900

    ✅ 마크다운 테스트와 구분자 없는 마크다운/블록 속성 확인 기능 추가

--- src/content-import/parser/content-parser.spec.ts
+++ src/content-import/parser/content-parser.spec.ts
@@ -1,5 +1,9 @@
 import { ContentParser } from "./content-parser";
-import { ContentType } from "./types/parsed-content.abstract";
+import {
+  ContentProperties,
+  ContentType,
+  getEnumKeyByValue,
+} from "./types/parsed-content.abstract";
 import {
   LaTeXBlockContent,
   LaTeXInlineContent,
@@ -22,7 +26,40 @@ describe("ContentParser", () => {
   it("should parse markdown content", () => {
     const content = `# Title\n\n## Subtitle\n\nSome content.`;
     const parsedContent = parser.parse(content);
-    expect(parsedContent).toEqual([]);
+    expect(parsedContent).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          content: "# Title",
+          contentType: ContentType.MARKDOWN,
+          properties: [ContentProperties.HAS_NEWLINE],
+        }),
+        expect.objectContaining({
+          content: "",
+          contentType: ContentType.MARKDOWN,
+          properties: [ContentProperties.HAS_NEWLINE],
+        }),
+        expect.objectContaining({
+          content: "## Subtitle",
+          contentType: ContentType.MARKDOWN,
+          properties: [ContentProperties.HAS_NEWLINE],
+        }),
+        expect.objectContaining({
+          content: "",
+          contentType: ContentType.MARKDOWN,
+          properties: [ContentProperties.HAS_NEWLINE],
+        }),
+        expect.objectContaining({
+          content: "Some content.",
+          contentType: ContentType.MARKDOWN,
+          properties: [ContentProperties.HAS_NEWLINE],
+        }),
+        expect.objectContaining({
+          content: "",
+          contentType: ContentType.MARKDOWN,
+          properties: [ContentProperties.HAS_NEWLINE],
+        }),
+      ])
+    );
   });
 
   it("should parse content with LaTeX", () => {
@@ -94,39 +131,74 @@ describe("ContentParser", () => {
   });
 
   it("누락된 블록 표현식을 인라인 표현식으로 표현하는 경우 파싱 확인", () => {
-    const originalContent =
-      "\\begin{case}\n a + 1 & a + 2 \n\\end{case}$$뒷내용$a + 1 $";
+    // const originalContent =
+    //   "\\begin{case}\n a + 1 & a + 2 \n\\end{case}$$뒷내용$a + 1 $";
+    const originalContent = `첫째항과 공비가 같고 공비가 1보다 큰 등비수열 $\\{a_n\\}$이 있다. 다음은 모든 자연수 $n$에 대하여 
+\\left( \\sum_{k=1}^{n} a_k \\right)^2 = \\sum_{k=1}^{n} \\frac{(a_2 + a_1) a_k^2 - 2a_1 a_k^2}{a_2 - a_1} \\quad \\cdots (*)
+이 성립함을 수학적 귀납법으로 증명한 것이다.
+등비수열 $\\{a_n\\}$의 공비를 $r$라 하면 $a_2 = a_1 r$ 이므로 $(*)$의 우변은
+\\sum_{k=1}^{n} \\frac{(r+1) a_k^2 - 2a_1 a_k^2}{a_2 - a_1} = \\sum_{k=1}^{n} \\frac{(r+1) a_k^2 - 2a_1 a_k^2}{a_2 - a_1} \\quad (가)
+(i) $n=1$일 때, 
+(좌변)$=($우변$)=a_1^2$
+따라서 $(*)$이 성립한다.
+(ii) $n=m$일 때, $(*)$이 성립한다고 가정하면
+\\left( \\sum_{k=1}^{m} a_k \\right)^2 = \\sum_{k=1}^{m} \\frac{(r+1) a_k^2 - 2a_1 a_k^2}{a_2 - a_1} \\quad (가)
+이다.
+\\begin{aligned}
+\\left( \\sum_{k=1}^{m+1} a_k \\right)^2 &= \\sum_{k=1}^{m+1} \\frac{(r+1) a_k^2 - 2a_1 a_k^2}{a_2 - a_1} \\quad (가) \\\\
+&= \\sum_{k=1}^{m} \\frac{(r+1) a_k^2 - 2a_1 a_k^2}{a_2 - a_1} + \\frac{(r+1) a_{m+1}^2 - 2a_{m+1} a_1}{a_2 - a_1} \\quad (가) \\\\
+&= \\left( \\sum_{k=1}^{m} a_k \\right)^2 + a_{m+1}^2 + \\boxed{\\text{(나)}} \\times a_{m+1} (a_{m+1} - a_1) \\\\
+&= \\left( \\sum_{k=1}^{m+1} a_k \\right)^2 
+\\end{aligned}
+$$
+따라서 $n=m+1$일 때도 $(*)$이 성립한다.
+(i), (ii)에 의하여 모든 자연수 $n$에 대하여 $(*)$이 성립한다.
+위의 (가), (나)에 알맞은 식을 각각 $f(r)$, $g(r)$라 하고, (다)에 알맞은 식에 $r=2$, $m=3$을 대입한 값을 $p$라 할 때, $\\dfrac{f(p)}{g(p+1)}$ 의 값은?
+① 484  ② 488  ③ 492  ④ 496  ⑤ 500`;
     const parsedContents = parser.parse(originalContent);
+    console.log(
+      parsedContents
+        .map((parsedContent) => {
+          const content = parsedContent.getContent();
+          const contentType = getEnumKeyByValue(
+            ContentType,
+            parsedContent.contentType
+          );
+          const properties = parsedContent
+            .getProperties()
+            .map((property) => getEnumKeyByValue(ContentProperties, property));
+          return `${content},${contentType},${properties.join("|")}`;
+        })
+        .join("\n")
+    );
     expect(parsedContents).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
-          content: "\\begin{case}",
+          content:
+            "\\left( \\sum_{k=1}^{n} a_k \\right)^2 = \\sum_{k=1}^{n} \\frac{(a_2 + a_1) a_k^2 - 2a_1 a_k^2}{a_2 - a_1} \\quad \\cdots (*)",
           contentType: ContentType.MARKDOWN,
+          properties: [
+            ContentProperties.HAS_TEX,
+            ContentProperties.HAS_NEWLINE,
+          ],
         }),
         expect.objectContaining({
-          content: " a + 1 & a + 2 ",
+          content: "\\begin{aligned}",
           contentType: ContentType.MARKDOWN,
+          properties: [
+            ContentProperties.HAS_BEGINNING_BLOCK,
+            ContentProperties.HAS_NEWLINE,
+          ],
         }),
         expect.objectContaining({
-          content: "\\end{case}",
+          content: "\\end{aligned}",
           contentType: ContentType.MARKDOWN,
-        }),
-        expect.objectContaining({
-          content: "뒷내용",
-          contentType: ContentType.LATEX_BLOCK,
-        }),
-        expect.objectContaining({
-          content: "a + 1 ",
-          contentType: ContentType.LATEX_INLINE,
-        }),
-        expect.objectContaining({
-          content: "",
-          contentType: ContentType.MARKDOWN,
+          properties: [
+            ContentProperties.HAS_ENDING_BLOCK,
+            ContentProperties.HAS_NEWLINE,
+          ],
         }),
       ])
     );
-    parsedContents.forEach((parsedContent) => {
-      console.log(parsedContent);
-    });
   });
 });
