07479b91da514355dd3eadfe1052eed92a2cf78a
BearMett <agomdole@gmail.com>
2024-10-11 18:35:38 +0900

    🚚 내부 모듈의 함수 이름이 변경되어서 사용하는 쪽에서도 반영

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import { getContentImporter } from "./src/content-import/content-importer";
+import { getCorrectedContents } from "./src/content-import/content-importer";
 
 function main() {
   console.log(
-    getContentImporter(`실수 t에 대하여 두 함수
+    getCorrectedContents(`실수 t에 대하여 두 함수
 $f(x)=(x-t)^2-2$, $g(x)=\\begin{cases} -x-1 & (x \\le 1) \\\\ x+1 & (x > 1) \\end{cases}$
 의 그래프가 만나는 서로 다른 점의 개수를 $h(t)$라 할 때,
 함수 $h(t)$를 $t$의 값의 범위에 따라 구하고 함수 $h(t)$가
